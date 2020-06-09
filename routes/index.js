9bc0e4fed14eb0b45fa5b2a91caaa16ae093e775
David Linner <david@linner.berlin>
2020-06-09 15:15:24 +0200

    Added article page.

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,33 @@ const db = require('../db/index');
 
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, response, next) {
 
   db.query('select article_pk, teaser, price, caption from articles', (err, result) => {
     if(err){
       next(err);
     }
 
-    res.render('index', { articles: result.rows });
+    response.render('index', { articles: result.rows });
+  })
+
+});
+
+router.get('/article/:id', function(req, res, next) {
+  const id = req.params.id;
+
+  db.query(`select article_pk, teaser, description, price, caption from articles where article_pk = ${id}`, (err, result) => {
+    if(err){
+      next(err);
+    }
+
+    if(result.rows.length < 1){
+      next();
+    } else {
+      const row = result.rows[0]; // const [row] = result.rows;
+
+      res.render('article', { article: row });
+    }
   })
 
 });
